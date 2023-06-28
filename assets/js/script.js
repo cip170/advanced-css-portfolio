61f51aa4910a1a56555e033664fce801747cc923
cip170 <charlottephilipps@gmail.com>
2023-06-28 15:05:02 +1000

    edited css, html & js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+/*DOMContentLoaded event ensures script runs after doc's finished loading. Height of header's retrievef using offSetHeight &
+selects all links inside header with href attr starting with #. When links are clicked, default behaviour is prevented & target
+element ID is retrieved, with target position calculated with getBoundingClientRect().top, window.scrollyY & header height, &
+scrolls to target pos with window.scrollTo()
+ */
 document.addEventListener("DOMContentLoaded", function() {
     var headerHeight = document.querySelector(".header").offsetHeight;
     var links = document.querySelectorAll(".header a[href^='#']");
@@ -8,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
             var targetId = link.getAttribute("href").substring(1);
             var targetElement = document.getElementById(targetId);
             if (targetElement) {
-                var targetPosition = targetElement.getBoundingClientRect().top + window.pageYOffset - headerHeight;
+                var targetPosition = targetElement.getBoundingClientRect().top + window.scrollY - headerHeight;
                 window.scrollTo({
                     top: targetPosition,
                     behavior: "smooth"
@@ -16,12 +21,14 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
-
-    var HeaderTitle = document.querySelector(".headerh1");
-    HeaderTitle.addEventListener("click", function() {
+/*Taking the user back to top of page when <h1>Charlotte Philipps</h1> is clicked by adding event listener to h1 element 
+with class headerTitle. Event listener is triggered when <h1> element is clicked with window.scrollTo() function being called
+with top: 0 so it scrolls to top of page with smooth scroll behaviour*/
+    var headerTitle = document.querySelector(".header h1");
+    headerTitle.addEventListener("click", function() {
         window.scrollTo({
-            top:0,
-            behaviour: "smooth"
+            top: 0,
+            behavior: "smooth"
         });
     });
 });
